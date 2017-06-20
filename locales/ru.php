0fe91bb2c437fc1576ff80e36237b341b250c5c0
Ivo Petkov <ivo@ivopetkov.com>
2017-06-20 16:07:42 +0300

    Added russian language with some translations.

---
+++ locales/ru.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'ivopetkov.form.This field is required.' => 'Это обязательное поле!',
+    'ivopetkov.form.The length of this field must be atleast %s characters.' => 'Това поле трябва да съдържа поне %s символа.',//todo
+    'ivopetkov.form.The length of this field must be atmost %s characters.' => 'Това поле може да съдържа най-много %s символа.',//todo
+    'ivopetkov.form.This is not a valid email address.' => 'Този имейл адрес не е валиден.',//todo
+    'ivopetkov.form.This is not a valid value.' => 'Тази стойност не е валидна.',//todo
+];
