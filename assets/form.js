6cb8ffbf4b946e1cf3c4d92d1fe8d297c6a98f4b
Ivo Petkov <ivo@ivopetkov.com>
2018-07-11 13:29:41 +0300

    Updated comments.

--- assets/form.js
+++ assets/form.js
@@ -1,7 +1,7 @@
 /*
  * Form addon for Bear Framework
  * https://github.com/ivopetkov/form-bearframework-addon
- * Copyright (c) 2016-2017 Ivo Petkov
+ * Copyright (c) Ivo Petkov
  * Free to use under the MIT license.
  */
 
