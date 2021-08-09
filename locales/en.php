c2d89145f59b96a57347429c4dc481adfbc91e18
Ivo Petkov <ivo@ivopetkov.com>
2021-08-09 07:32:58 +0300

    Update error messages.

--- locales/en.php
+++ locales/en.php
@@ -1,10 +1,11 @@
 <?php
 
 return [
-    'ivopetkov.form.This field is required.' => 'This field is required.',
-    'ivopetkov.form.The length of this field must be atleast %s characters.' => 'The length of this field must be atleast %s characters.',
-    'ivopetkov.form.The length of this field must be atmost %s characters.' => 'The length of this field must be atmost %s characters.',
-    'ivopetkov.form.This is not a valid email address.' => 'This is not a valid email address.',
-    'ivopetkov.form.This is not a valid value.' => 'This is not a valid value.',
-    'ivopetkov.form.Error occurred. Please, try again later.' => 'Error occurred. Please, try again later.',
+    'ivopetkov.form.This field is required.' => 'This field is required!',
+    'ivopetkov.form.The length of this field must be atleast %s characters.' => 'The length of this field must be atleast %s characters!',
+    'ivopetkov.form.The length of this field must be atmost %s characters.' => 'The length of this field must be atmost %s characters!',
+    'ivopetkov.form.This is not a valid email address.' => 'This is not a valid email address!',
+    'ivopetkov.form.This is not a valid phone number.' => 'This is not a valid phone number!',
+    'ivopetkov.form.This is not a valid value.' => 'This is not a valid value!',
+    'ivopetkov.form.Error occurred. Please, try again later.' => 'Error occurred! Please, try again later.',
 ];
--- locales/ru.php
+++ locales/ru.php
@@ -2,9 +2,10 @@
 
 return [
     'ivopetkov.form.This field is required.' => 'Это обязательное поле!',
-    'ivopetkov.form.The length of this field must be atleast %s characters.' => 'Това поле трябва да съдържа поне %s символа.', //todo
-    'ivopetkov.form.The length of this field must be atmost %s characters.' => 'Това поле може да съдържа най-много %s символа.', //todo
-    'ivopetkov.form.This is not a valid email address.' => 'Този имейл адрес не е валиден.', //todo
-    'ivopetkov.form.This is not a valid value.' => 'Тази стойност не е валидна.', //todo
-    'ivopetkov.form.Error occurred. Please, try again later.' => 'Възникна грешка. Моля, опитайте отново малко по-късно.', //todo
+    'ivopetkov.form.The length of this field must be atleast %s characters.' => 'Длина этого поля должна быть не менее %s символов!',
+    'ivopetkov.form.The length of this field must be atmost %s characters.' => 'Длина этого поля не должна превышать %s символов!',
+    'ivopetkov.form.This is not a valid email address.' => 'Это недействительный адрес электронной почты!',
+    'ivopetkov.form.This is not a valid phone number.' => 'Это недействительный номер телефона!',
+    'ivopetkov.form.This is not a valid value.' => 'Это неверное значение !',
+    'ivopetkov.form.Error occurred. Please, try again later.' => 'Произошла ошибка! Пожалуйста, повторите попытку позже.',
 ];
