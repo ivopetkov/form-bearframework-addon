7a71a2a337ae1f4dd9d58bf82fe51cbd44db0d09
Ivo Petkov <ivo@ivopetkov.com>
2021-12-01 17:44:13 +0200

    Update FormTest.php

--- tests/FormTest.php
+++ tests/FormTest.php
@@ -18,7 +18,20 @@ class FormTest extends BearFramework\AddonTests\PHPUnitTestCase
      */
     public function testForm()
     {
-        $this->assertTrue(true);
-    }
+        $app = $this->getApp();
+        $tempDir = $this->getTempDir();
 
+        $formFilename = $tempDir . '/form1.php';
+        file_put_contents($formFilename, '<?php
+
+echo \'<form>\';
+echo \'<input type="text" name="key1"/>\';
+echo \'</form>\';
+
+');
+
+        $html = $app->components->process('<component src="form" filename="' . htmlentities($formFilename) . '">');
+        $this->assertTrue(strpos($html, '<form data-form-id') !== false);
+        $this->assertTrue(strpos($html, '<input type="text" name="key1">') !== false);
+    }
 }
